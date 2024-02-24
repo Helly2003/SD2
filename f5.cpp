d9ef6cf7e1d18af9f00bbe228101be3b81738ef7
Helly Shah <shahhelly2193@gmail.com>
2024-02-23 20:59:16 -0500

    first commit for f5 without the functions created

---
+++ f5.cpp
@@ -0,0 +1,21 @@
+/*
+* File : f5.cpp
+* Project : Focused Assignment 5
+* Programar : Helly Shah
+* Submission Date : 2024-02-23
+* Description :
+*/
+#include <stdio.h>
+int getNum();
+void modifyArrayValues(int numbers[], int size);
+int maxArrayValue(int numbers[], int size);
+
+int main()
+{
+	int numbers[10] = { 0 };
+	modifyArrayValue(numbers, 10);
+	int index = maxArrayValue(numbers, 10);
+	{
+		printf("The highest value is %d at index %d", );
+	}
+}
